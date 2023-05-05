9249f49858fd1206f1e1a7d507f5c709b095cd86
Enzo Romano Panebianco - 821224441 <821224441@ulife.com.br>
2023-05-05 20:03:14 -0300

    Lista de plugins

--- frontend/src/pages/ListaDePlugins/index.jsx
+++ frontend/src/pages/ListaDePlugins/index.jsx
@@ -1,9 +1,106 @@
 import React from 'react'
-
+import './styles.css'
+import Img from '../../assets/flstudio.png'
 function ListaDePlugins() {
   return (
     <>
-        <h1>Lista de plugins</h1>
+      <div className='container-plugins'>
+        <main className="content-plugins">
+          <h1 className='title-plugins'>Lista de plugins</h1>
+          <div className='card-list-plugins'>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+            <div className='card-item-plugins'>
+              <header>
+                <img src={Img} width="100px" alt="" />
+                <strong>Plugin 1</strong>
+              </header>
+              <footer>
+                <button>Ver plugin</button>
+              </footer>
+            </div>
+          </div>
+        </main>
+      </div>
     </>
   );
 }

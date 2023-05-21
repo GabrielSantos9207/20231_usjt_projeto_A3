9a145241c5ec705c2777f74849d6cd123cad6f81
enzopanebianco <enzinho983@gmail.com>
2023-05-21 18:49:17 -0300

    Implmentada funcionalidade de avaliação de plugin

--- frontend/src/pages/Homepage/index.jsx
+++ frontend/src/pages/Homepage/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import "../assets/style.css";
 import "../../assets/style.css"
+import { getPlugins } from '../../services/api';
+import Header from '../../components/Header';
 
-function Homepage()  {
-
+function Homepage() {
+  const [plugins, setPlugins] = useState([])
+  const navigate = useNavigate()
+  const username = localStorage.getItem('user-nome')
   const multiComp = {
     pluginId: 1,
     titulo: "C6 Multiband Compressor",
@@ -13,54 +16,50 @@ function Homepage()  {
     imagem: "../../assets/c6-multiband-compressor.avif"
   }
 
-  return(
-  <div className="row vh-100 d-flex" class="black">
-    <div className="col-12">
+  useEffect(() => {
+    async function listarPlugins() {
+      const data = await getPlugins()
+      setPlugins(data)
+    }
+    listarPlugins()
+  }, [])
 
-      <div className="mx-auto justify-content-center align-items-center">
-        <h1>SoftMusi</h1>
-      </div>
+  return (
+    <div className="row vh-100 d-flex" class="black">
+      <div className="col-12">
 
-      <hr></hr>
-
-      <nav className='w-100 justify-content-center align-items-center' class="login">
-        <Link to={{ pathname: '/login'}}>
-          <button type="button" className="btn btn-lg btn-block" class ="user">User</button>
-          <button type="button" className="btn btn-lg btn-block" class ="user">Sign out</button>
-        </Link>
-      </nav>
-
-      <hr></hr>
-
-      <div class="p-card-d">
-      <p class="description">
-       Bem-vindo ao nosso site de avaliação de plugins VST! Com tantas opções disponíveis, pode ser difícil saber quais plugins VST realmente valem a pena investir, é aqui que nós entramos. Nosso site é dedicado para que os usúarios possam contribuir com suas próprias avaliações sobre os plugins que usaram. Se você é um produtor musical que procura os melhores plugins VST para aprimorar sua produção, explore nosso site e compartilhe suas opiniões. Juntos, podemos ajudar a criar uma comunidade mais informada e capacitada de criadores musicais.</p>
-       </div>
-
-      <br></br>
-
-      <h2 id="vst-title">VST Plugins:</h2>
-      <div class="plugins">
-        <div class="p-card-1">
-          <Link to={{ pathname: '/plugin', search: `pID=${multiComp.pluginId}&titulo=${multiComp.titulo}&descricao=${multiComp.descricao}&imagem=${multiComp.imagem}` }}>
-          <img src= {`${multiComp.imagem}`} alt='C6 Multiband Compressor' className='img-fluid' class="p-img"></img>
-          <h3 class="p-text">C6 Multiband Compressor</h3>
-          </Link>
+        <div className="mx-auto justify-content-center align-items-center">
+          <h1>SoftMusi</h1>
         </div>
-        <div class="p-card-2">
-          <img src='../../assets/waves-tune-real-time.avif' alt='Waves Tune Real Time' className='img-fluid' class="p-img"></img>
-          <h3>Waves Tune Real Time</h3>
+
+        <Header />
+        <div class="p-card-d">
+          <p class="description">
+            Bem-vindo ao nosso site de avaliação de plugins VST! Com tantas opções disponíveis, pode ser difícil saber quais plugins VST realmente valem a pena investir, é aqui que nós entramos. Nosso site é dedicado para que os usúarios possam contribuir com suas próprias avaliações sobre os plugins que usaram. Se você é um produtor musical que procura os melhores plugins VST para aprimorar sua produção, explore nosso site e compartilhe suas opiniões. Juntos, podemos ajudar a criar uma comunidade mais informada e capacitada de criadores musicais.</p>
         </div>
+
+        <br></br>
+
+        <h2 id="vst-title">VST Plugins:</h2>
+        <div class="plugins">
+          {plugins.map(plugin =>
+            <div onClick={() => navigate(`/plugin/${plugin.id}`)} key={plugin.id} className="p-card-1">
+              <img className='p-img' src={plugin.imagem} />
+              <h3>{plugin.titulo}</h3>
+              <span>{plugin.descricao}</span>
+            </div>
+
+          )}
+        </div>
+
+        <hr></hr>
+
+        <footer class="footer">
+          <p class="footer-text">SoftMusi Copyright © 2023</p>
+        </footer>
+
       </div>
-
-      <hr></hr>
-
-      <footer class="footer">
-        <p class="footer-text">SoftMusi Copyright © 2023</p>
-      </footer>
-
     </div>
-  </div>
   )
 }
 
--- frontend/src/pages/PlugInModel/index.jsx
+++ frontend/src/pages/PlugInModel/index.jsx
@@ -1,61 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Estrela from "../../components/LayoutComponents/estrela"
-import { useLocation, Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
-//import "../assets/style.css";
 import "../../assets/style.css"
+import { avaliarPlugin, getPlugin } from '../../services/api';
+import Header from '../../components/Header';
 
 const PluginPage = () => {
+  const { id } = useParams()
+  const [plugin, setPlugin] = useState({})
+  const [comentario, setComentario] = useState('')
+  const [nota, setNota] = useState('0')
+  const usuarioId = localStorage.getItem('user-id')
+  const usuarioLogado = usuarioId!==null && usuarioId.trim() !== ''
 
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  //const pID = params.get('pID');
-  const titulo = params.get('titulo');
-  const descricao = params.get('descricao');
-  const imagem = params.get('imagem');
+  function handleAvaliarPlugin(e) {
+    e.preventDefault()
+    const avaliacao = {
+      comentario,
+      nota,
+      usuarioId
+    }
+    avaliarPlugin(id, avaliacao)
 
-  return(
-  <div className="row vh-100 d-flex" class="black">
-    <div className="col-12">
+    window.alert('Comentário enviado')
+    listarPlugin()
 
-      <div className="mx-auto justify-content-center align-items-center">
-        <h1>SoftMusi</h1>
-      </div>
+  }
+  async function listarPlugin() {
+    const data = await getPlugin(id)
+    setPlugin(data)
+  }
 
-      <hr></hr>
+  useEffect(() => {
+    listarPlugin()
+  }, [id])
+  return (
+    <div className="row vh-100 d-flex" class="black">
+      <div className="col-12">
 
-      <nav className='w-100 justify-content-center align-items-center' class="login">
-        <Link to={{ pathname: '/login'}}>
-          <button type="button" className="btn btn-lg btn-block" class ="user">User</button>
-          <button type="button" className="btn btn-lg btn-block" class ="user">Sign out</button>
+        <div className="mx-auto justify-content-center align-items-center">
+          <h1>SoftMusi</h1>
+        </div>
+        <Header />
+        <Link to={{ pathname: "/" }}>
+          <h2 id="return"><i class="bi bi-arrow-left">Voltar</i>
+          </h2>
         </Link>
-      </nav>
 
-      <hr></hr>
+        <br></br>
 
-      <Link to={{pathname:"/" }}>
-          <h2 id="return">&#10149;Return to Homepage</h2>
-      </Link>
+        <div class="plugin-container">
+          <div className='w-50 text-white d-flex flex-column align-items-center'>
+            <img src={plugin.imagem} alt={plugin.titulo} className='img-fluid' ></img>
+            <div className='d-flex align-items-center my-2 gap-4'>
+              <h2 className='text-white'>{plugin.titulo}</h2>
+              <span className='fs-3 text-warning'>{plugin.notaGeral}
+                <i class="bi bi-star-fill"></i>
+              </span>
+            </div>
+            <span class="p-description my-2">{plugin.descricao}</span>
+            <span>Link para baixar: {plugin.link}</span>
+          </div>
+          <div className='d-flex flex-column align-items-center w-75'>
+            {usuarioLogado &&
+              <form onSubmit={handleAvaliarPlugin}>
+                <Estrela nota={nota} setNota={setNota} />
+                <input
+                  onChange={({ target }) => setComentario(target.value)}
+                  className='w-75 my-2 form-control'
+                  type="text" placeholder={'Comente sobre o ' + plugin.titulo} />
+                <button type='submit' disabled={comentario.trim() === ''}
+                  className='btn w-75 btn-info text-white my-2' >Enviar</button>
+              </form>
+            }
+            <h4 className='my-4 text-white'>Comentários</h4>
+            <div className='w-100 gap-2 d-flex flex-column justify-content-between' style={{ maxHeight: '300px', overflowY: 'scroll' }}>
+              {plugin.avaliacoes?.map(avaliacao =>
+                <div className='row gap-2 avaliacao align-items-center' key={avaliacao.id}>
+                  <div className='d-flex col flex-column text-white'>
+                    <i class="bi bi-person-fill"></i>
+                    <strong>{avaliacao.usuario?.nome}</strong>
+                  </div>
+                  <p className='col-8  text-white-50'>{avaliacao.comentario}</p>
+                  <span className='col text-warning d-flex gap-1 align-items-center'>
+                    {avaliacao.nota}
+                    <i class="bi bi-star-fill"></i>
 
-      <br></br>
+                  </span>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
 
-      <div class="p-card-1">
-        <img src= {imagem} alt= {titulo} className='img-fluid' ></img>
-        <h2>{titulo}</h2>
-        
-        <p class="p-description">{descricao}</p>
 
-        <Estrela />
+
 
       </div>
-
-      
-      <footer class="footer">
-        <p class="footer-text">SoftMusi Copyright © 2023</p>
-      </footer>
-
     </div>
-  </div>
   )
 }
 
